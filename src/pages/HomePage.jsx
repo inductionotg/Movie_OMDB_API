c44d6b2adf47d5b71e28e775f8bf08f88d1b6020
inductionotg <ritesh2000.sinha@gmail.com>
2024-05-30 11:24:30 +0530

    Parralel movie download

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react"
+import MovieCard from "../components/MovieCard"
+import {searchMovie} from '../apis/omdb'
+import axios from "axios"
 import "./HomePage.css"
 function HomePage(){
+    const [moviedata,setMovieData] = useState([])
+    async function fetchMovie(...args){
+        const urls = args.map((name)=>searchMovie(name))
+        console.log("dsd",urls)
+        try {
+            const response = await axios.all(urls.map((url)=>axios.get(url)))
+            const responseMovies = response.map((movies)=>movies.data.Search)
+            console.log(responseMovies)
+            setMovieData([].concat(...responseMovies))
+        } catch (error) {
+            console.log("Error",error)
+        }
+    }
+    useEffect(()=>{
+        fetchMovie('harry',"batman","avengers")
+    },[])
     return (
         <>
-            HomePage
+            <div className="movie-card-wrapper">
+                {moviedata.length>0 && moviedata.map((movie)=>{
+                    return (
+                        <MovieCard key={movie?.imdbID} {...movie}/>
+                    )
+                })}
+            </div>
         </>
     )
 }
